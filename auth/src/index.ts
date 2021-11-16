633776a4c2f6318d74bb982a431c6ad59649baff
Javier Saiz <javiersaizcano@gmail.com>
2021-11-16 19:59:53 +0100

    add ingress

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,11 @@ const app = express();
 app.use(express.json())
 
 
+app.get('/api/users/currentuser',(req, res) => {
+    res.send('Hi')
+})
+
+
 app.listen(3000,()=>{
     console.log('Listening on port 3000!!!!')
 })
