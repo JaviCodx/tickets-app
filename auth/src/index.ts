ed9e5f26b69cbd5269318d2fa6a4fdb4958796fe
Javier Saiz <javiersaizcano@gmail.com>
2021-11-18 18:40:31 +0100

    add 404 error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,18 +1,23 @@
 import express from 'express'
+import 'express-async-errors'
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signupRouter } from './routes/signup'
 import { signoutRouter } from './routes/signout'
 import { errorHandler } from './middlewares/error-handler'
+import { NotFoundError } from './errors/not-found-error'
 
 const app = express()
-
 app.use(express.json())
 
 app.use(currentUserRouter)
 app.use(signinRouter)
 app.use(signupRouter)
 app.use(signoutRouter)
+
+app.all('*', async () => {
+  throw new NotFoundError()
+})
 app.use(errorHandler)
 
 app.listen(3000, () => {
