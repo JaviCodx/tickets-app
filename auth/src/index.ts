317ba8f8c6ec3b4892496609f6060020f85c3c2a
Javier Saiz <javiersaizcano@gmail.com>
2021-11-18 19:49:50 +0100

    add auth mongodb

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import 'express-async-errors'
+import mongoose from 'mongoose'
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signupRouter } from './routes/signup'
@@ -20,6 +21,16 @@ app.all('*', async () => {
 })
 app.use(errorHandler)
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!!!!')
-})
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {})
+    console.log('Connected to MongoDB')
+  } catch (err) {
+    console.log(err)
+  }
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!')
+  })
+}
+
+start()
