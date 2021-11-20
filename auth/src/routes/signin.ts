e1ad598f2bc4714f4d3c3306c8d6ba1c5917a4c2
Javier Saiz <javiersaizcano@gmail.com>
2021-11-20 16:03:05 +0100

    add signin

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,47 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
+import { body } from 'express-validator'
+import { User } from '../models/user'
+import { validateRequest } from '../middlewares/validate-request'
+import { BadRequestError } from '../errors/bad-request-error'
+import { Password } from '../services/password'
 
 const router = express.Router()
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().notEmpty().withMessage('Password must be supplied')
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password }: { email: string; password: string } = req.body
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('Hi there')
-})
+    const existingUser = await User.findOne({ email: email })
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials')
+    }
+
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    )
+    if (!passwordsMatch) {
+      throw new BadRequestError('Invalid credentials')
+    }
+
+    //Generate jwt token
+    const userJwt = jwt.sign(
+      { id: existingUser.id, email: existingUser.email },
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      process.env.JWT_KEY!
+    )
+
+    //Store in cookieSession
+    req.session = { jwt: userJwt }
+
+    res.status(201).send(existingUser)
+  }
+)
 
 export { router as signinRouter }
