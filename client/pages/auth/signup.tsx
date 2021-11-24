a4b49a29a173e0b753cabc827ba83840b4af9b08
Javier Saiz <javiersaizcano@gmail.com>
2021-11-24 20:04:30 +0100

    add signup form

--- client/pages/auth/signup.tsx
+++ client/pages/auth/signup.tsx
@@ -1,25 +1,40 @@
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
 import { useState } from 'react';
 
 const Signup = () => {
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <>
       <Box
-        component="form"
-        sx={{
-          '& .MuiTextField-root': { m: 1, width: '25ch' },
-        }}
-        noValidate
-        autoComplete="off"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"
       >
         <TextField
           onChange={(e) => setEmail(e.target.value)}
           required
           id="outlined-required"
-          label="Required"
+          label="Email"
+          value={email}
+          helperText="Please enter your email"
+          type="email"
         />
+
+        <TextField
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          id="outlined-required"
+          label="Password"
+          value={password}
+          helperText="Please enter your password"
+          type="password"
+        />
+        <Button variant="contained">Submit</Button>
       </Box>
     </>
   );
