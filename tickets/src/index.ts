18c675902c8102c7753b763c7d1f8d26479a36ce
Javier Saiz <javiersaizcano@gmail.com>
2021-12-12 10:48:38 +0100

    provide MONGO_URI as env

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,8 +5,12 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be provided in the environment')
   }
+
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be provided in the environment')
+  }
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {})
+    await mongoose.connect(process.env.MONGO_URI, {})
     console.log('Connected to MongoDB')
   } catch (err) {
     console.log(err)
